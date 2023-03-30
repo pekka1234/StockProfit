54a1f5d2874fa0af753ddbd920a08a2cd340bb05
pekka <pekkakulmala123@gmail.com>
2023-03-30 18:37:36 +0300

    sounds

--- main.js
+++ main.js
@@ -474,3 +474,23 @@ function save(){
     alert("Progress saved! You can exit now");
     window.onbeforeunload = s => false ? "" : null;
 }
+
+// infinitely playing audios
+var audio = new Audio("audio/" + names[cur].toString() + ".mp3");
+var check2 = 0;
+function sound(check){
+    if(check == check2 == 0 || check == check2 == 1){
+        check2 = 0;
+        audio.pause();
+        audio = new Audio("audio/" + names[cur].toString() + ".mp3");
+        audio.play();
+        audio.addEventListener("ended", function(){
+            check2 = 1;
+            audio.currentTime = 0;
+            sound(1);
+        });
+    }
+}
+
+
+    
